fe78fade362cad0edb102bacf6a2125824894870
Md Kamruzzaman Tusher <8888749+tkamruzzaman@users.noreply.github.com>
2023-09-03 20:24:01 +0600

    kitchen object SO

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform spawnPoint;
+
     public void Interact()
     {
         print("Interact");
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, spawnPoint);
+        kitchenObjectTransform.localPosition = Vector3.zero;
+
+        print(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
     }
 }

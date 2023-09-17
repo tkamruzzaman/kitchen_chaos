8d2dbd55c240e11e64b6d84aa0fad09d50902ddf
Md Kamruzzaman Tusher <8888749+tkamruzzaman@users.noreply.github.com>
2023-09-17 13:41:28 +0600

    plate counter

---
+++ Assets/Scripts/Counters/PlatesCounter.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatesCounter : BaseCounter
+{
+    public event EventHandler OnPlateSpawned;
+    public event EventHandler OnPlateRemoved;
+
+    [SerializeField] private KitchenObjectSO plateKitchenObjectSO;
+    private float spawnPlateTimer;
+    private float spawnPlateTimerMax = 4f;
+    private int plateSpawnedAmount;
+    private int plateSpawnedAmountMax = 4;
+
+    private void Update()
+    {
+        spawnPlateTimer += Time.deltaTime;
+
+        if(spawnPlateTimer > spawnPlateTimerMax)
+        {
+            spawnPlateTimer = 0;
+
+            //KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, this);
+        if(plateSpawnedAmount < plateSpawnedAmountMax)
+            {
+                plateSpawnedAmount++;
+
+                OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+            }
+        
+        }
+    }
+
+    public override void Interact(Player player)
+    {
+        if (!player.HasKitchenObject())
+        {
+            //player is empty handed
+            if(plateSpawnedAmount > 0) { 
+            //there is al least one plate here
+            plateSpawnedAmount--;
+                KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, player);
+                OnPlateRemoved?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public override void InteractAlternate(Player player)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Counters/PlatesCounterVisual.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatesCounterVisual : MonoBehaviour
+{
+    [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private Transform plateVisualPrefab;
+
+    private PlatesCounter platesCounter;
+    private List<GameObject> plateVisualGameObjectList = new List<GameObject>(); 
+
+    private void Start()
+    {
+        platesCounter = GetComponentInParent<PlatesCounter>();
+        platesCounter.OnPlateSpawned += PlatesCounter_OnPlateSpawned;
+        platesCounter.OnPlateRemoved += PlatesCounter_OnPlateRemoved;
+    }
+
+    private void PlatesCounter_OnPlateSpawned(object sender, System.EventArgs e)
+    {
+        Transform plateVisualTransform = Instantiate(plateVisualPrefab, counterTopPoint);
+        float plateOffsetY = 0.1f;
+        plateVisualTransform.localPosition = new Vector3(0, plateOffsetY * plateVisualGameObjectList.Count, 0);
+
+        plateVisualGameObjectList.Add(plateVisualTransform.gameObject);
+    }
+
+    private void PlatesCounter_OnPlateRemoved(object sender, System.EventArgs e)
+    {
+        GameObject plateGameObject = plateVisualGameObjectList[plateVisualGameObjectList.Count - 1];
+        
+        plateVisualGameObjectList.Remove(plateGameObject);
+
+        Destroy(plateGameObject);
+    }
+}

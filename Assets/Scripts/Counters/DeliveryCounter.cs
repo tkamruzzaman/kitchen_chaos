db303f710d664c1e0263cc4fd50873d8726c007e
Md Kamruzzaman Tusher <8888749+tkamruzzaman@users.noreply.github.com>
2023-09-17 18:29:24 +0600

    delivery counter visual

---
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if (player.HasKitchenObject())
+        {
+            if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+            {
+                //Only accepts Plates
+                player.GetKitchenObject().DestroySelf();
+            }
+        }
+    }
+
+    public override void InteractAlternate(Player player)
+    {
+    }
+}
